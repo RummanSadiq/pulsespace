1d67a86aecaea2543dab8ac8cd99892166d734d3
Rumman Sadiq <chrumman@gmail.com>
2019-05-25 02:31:31 +0500

    Update ShopFollower.php

--- app/ShopFollower.php
+++ app/ShopFollower.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ShopFollower extends Model
 {
     protected $fillable = [
-        "user_id", "store_id", "is_active"
+        "user_id", "shop_id", "is_active"
     ];
 
     public function user()
