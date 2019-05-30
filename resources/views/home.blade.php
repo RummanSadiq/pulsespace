96d39cfc86e538048cc7cd38bee4d1af5a622072
Rumman Sadiq <chrumman@gmail.com>
2019-05-30 19:23:26 +0500

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,22 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-        
-    </head>
-    <body>
-        <div id="app">
-            <script src="{{ asset('js/App.js') }}" ></script>
-        </div>
-    </body>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <div id="app">
+        <script type="text/jsx" src="{{ asset('js/App.js') }}"></script>
+    </div>
+</body>
+
 </html>
